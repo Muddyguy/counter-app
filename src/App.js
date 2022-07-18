64361e3ba70068c78b5f1a1445c4cd407d22dbd4
muddyguy <mudassar.hussain360@gmail.com>
2022-07-18 12:42:47 -0400

    counter stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+
+let counts = 0;
+function ClicksMe() {
+   counts ++
+   document.getElementById("counts").innerHTML = counts;
+}
+function DontClicksMe() {
+  counts --
+  document.getElementById("counts").innerHTML = counts;
+
+}
+function MaybeClicksMe() {
+  counts = 0
+  document.getElementById("counts").innerHTML = counts;
+
+}
+function DefClicksMe() {
+  counts = counts +10
+  document.getElementById("counts").innerHTML = counts;
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1> Yerr supersmash bros</h1>
+        <button onClick={ClicksMe}>SMASH</button>
+
+
+
+        <button onClick={DontClicksMe}>PASS</button>
+        <button onClick={MaybeClicksMe}>KO</button>
+        <button onClick={DefClicksMe}>SUPER SMASH</button>
+
+
+        <p> Touches <a id = "counts"> 0</a></p>
+      
     </div>
   );
 }
